d833bf466e2b001daaa932f0bb4a504fd5cf52ad
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-06-04 18:47:36 +0530

    Added script to search big files over disk

--- FindBigFiles.sh
+++ FindBigFiles.sh
@@ -7,6 +7,6 @@ if [ "$#" -ne 2 ]
 then
         echo "Please enter 1.File extension and 2.File size through command line!!"
 else
-        sudo find / -type f -name '*.'$fileExtension'' -size +50M -exec ls -sh {} \;
+	sudo find / -type f -name '*.'zip'' -size +50M | xargs ls -ltr -h | awk '{print $5,$6,$7,$8,$9}'
 fi
 
