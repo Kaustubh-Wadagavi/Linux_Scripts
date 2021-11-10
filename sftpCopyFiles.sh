82e03f94541ee71a0cc6a0c26a42e94a4a5e5324
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-11-10 18:59:32 +0530

    Date and errors log file is added

--- sftpCopyFiles.sh
+++ sftpCopyFiles.sh
@@ -1,5 +1,22 @@
 #! /bin/bash
 
+LOG_FILE=copy.log
+
+backupStartTime() {
+    echo "-----------------------------------------------------" &>> $LOG_FILE
+    echo "Copying Start Time:" `date +%x-%r` &>> $LOG_FILE
+}
+
+backupEndTime() {
+    echo "Copying End Time:" `date +%x-%r` &>> $LOG_FILE
+    echo "-----------------------------------------------------" &>> $LOG_FILE
+}
+backupFailedTime() {
+    echo "Copying failed time:" `date +%x-%r` &>> $LOG_FILE
+}
+
+backupStartTime
+
 LOCK_FILE=/tmp/print_lock.txt 
 
 if [ -e $LOCK_FILE ] && kill -0 `cat $LOCK_FILE`; then
@@ -14,13 +31,17 @@ for SUB_DIR in `ls -d /usr/local/openspecimen/data/print-labels/*`
 do
   for FILE in `ls -tr $SUB_DIR/*`        
   do
-   sudo scp -i /home/krishagni/Desktop/sftp-scripts/jenkins-private $FILE jenkins@build.openspecimen.org:/home/jenkins/test
+
+   sudo scp -i /home/krishagni/Desktop/sftp-scripts/jenkins-private $FILE jenkins@build.openspecimen.orgs:/home/jenkins/test &>> $LOG_FILE
+
     if [ $? -ne 0 ]; then
-      echo "Failed to copy the file"  
+      backupFailedTime
     else
-      echo "File copied successfully"            
+      backupEndTime            
     fi
   done
 done
 
 rm $LOCK_FILE
+
+
