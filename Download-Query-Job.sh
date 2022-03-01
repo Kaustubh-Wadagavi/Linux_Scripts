4ff8e2379d5d4afc072c01c56637c86e14385dbf
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2022-03-01 17:17:08 +0530

    All changes are done

--- Download-Query-Job.sh
+++ Download-Query-Job.sh
@@ -5,13 +5,16 @@ PASSWORD='Login@123'                                        # Password of the us
 JOB_ID=23                                                   # Id of the JOB.
 URL="http://localhost:8080/openspecimen/"                   # Host Url.
 
-getCSV() {
+getCSV() {	
  JOB_DETAILS=$(curl -H "X-OS-API-TOKEN: $TOKEN" -H "Content-Type: application/json" --request GET "$URL/rest/ng/scheduled-jobs/$JOB_ID/runs")
  
- JOB_RUN_ID=`echo ${JOB_DETAILS} | jq -r '.[0].id'`
- JOB_RUN_STATUS=`echo ${JOB_DETAILS} | jq -r '.[0].status'`
+ JOB_RUN_ID_AND_STATUS=`echo ${JOB_DETAILS} | jq -r '.[0].id,.[0].status'`
  
- if [[ ! -z "$JOB_RUN_ID" ]] && [[ "$JOB_RUN_STATUS" = "SUCCEEDED" ]]
+ JOB_RUN_ID=$(echo $JOB_RUN_ID_AND_STATUS | tr -d -c 0-9)
+ 
+ JOB_RUN_STATUS=$(echo $JOB_RUN_ID_AND_STATUS | tr -d '0123456789 ')
+ 
+ if [[ "$JOB_RUN_STATUS" = "SUCCEEDED" ]]
  then
     curl -H "X-OS-API-TOKEN: $TOKEN" -H "Content-Type: application/json" --request GET "$URL/rest/ng/scheduled-jobs/$JOB_ID/runs/$JOB_RUN_ID/result-file" >> scheduled_query_"$JOB_ID"_"$JOB_RUN_ID".csv
     exit 0;
@@ -19,7 +22,7 @@ getCSV() {
  then
     echo "Please wait the execution is running..."
     sleep 10;
-    getJobIdAndStatus
+    getCSV
  fi
 }
 
