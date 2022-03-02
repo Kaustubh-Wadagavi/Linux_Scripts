99131cf07f45fb0aace539c0243e4503529d09a4
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2022-03-02 14:14:24 +0530

    The bug is fixed

--- Download-Query-Job.sh
+++ Download-Query-Job.sh
@@ -14,20 +14,29 @@ getCSV() {
  
  JOB_RUN_STATUS=$(echo $JOB_RUN_ID_AND_STATUS | tr -d '0123456789 ')
  
- if [[ "$JOB_RUN_STATUS" = "SUCCEEDED" ]]
+ if [[ "$JOB_RUN_STATUS" = "SUCCEEDED" ]] && [[ $LAST_JOB_RUN_ID -lt $JOB_RUN_ID ]]
  then
     curl -H "X-OS-API-TOKEN: $TOKEN" -H "Content-Type: application/json" --request GET "$URL/rest/ng/scheduled-jobs/$JOB_ID/runs/$JOB_RUN_ID/result-file" >> scheduled_query_"$JOB_ID"_"$JOB_RUN_ID".csv
     exit 0;
- elif [[ "$JOB_RUN_STATUS" -eq "IN_PROGRESS" ]]
+ elif [[ "$JOB_RUN_STATUS" = "IN_PROGRESS" ]]
  then
     echo "Please wait the execution is running..."
     sleep 10;
     getCSV
+ elif [[ $LAST_JOB_RUN_ID = $JOB_RUN_ID ]]
+ then
+    echo "The job is not executed please check..."
+    exit 0;
  fi
 }
 
 executeJob() {
   curl -H "X-OS-API-TOKEN: $TOKEN" -H "Content-Type: application/json" --request POST --data '{ }' "$URL/rest/ng/scheduled-jobs/$JOB_ID/runs"
+}
+
+getLastExecutedJobRunID() {
+  LAST_EXECUTION_DETAILS=$(curl -H "X-OS-API-TOKEN: $TOKEN" -H "Content-Type: application/json" --request GET "$URL/rest/ng/scheduled-jobs/$JOB_ID/runs")
+  LAST_JOB_RUN_ID=`echo ${LAST_EXECUTION_DETAILS} | jq -r '.[0].id'`
 }
 
 getToken() {
@@ -37,5 +46,6 @@ getToken() {
 }
 
 getToken
+getLastExecutedJobRunID
 executeJob
 getCSV
