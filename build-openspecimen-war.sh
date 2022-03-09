60684e45aaa44a64dc4d8f33af12c4a3c363aec1
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2022-03-09 20:47:51 +0530

    Changes are done

--- build-openspecimen-war.sh
+++ build-openspecimen-war.sh
@@ -3,54 +3,44 @@
 branch_name=$1
 release_name=$2
 master_dir="/mnt/volume_nyc1_01/new_workspace/os_source/openspecimen"
-os_core="/mnt/volume_nyc1_01/new_workspace/os_source/openspecimen_core"
+
+if [ -z $branch_name ]
+then
+   echo "1. If you releasing only master build the please provide only master branch name with command line"
+   echo "2. If you releasing the version the please provide a. branch name b. release name with command line"
+   exit 0;
+fi
 
 npm cache clean --force
 /home/jenkins/jenkins_home/workspace/os_source/jenkins_script/remove-links.sh
 
-if [ "$branch_name" = "$release_name" ]
+cd $master_dir
+
+exists=`git ls-remote --heads origin $branch_name`
+if [ ! -n "$exists" ]
 then
-   cd $master_dir
-   git checkout $brach_name
-   git pull
-   cd $master_dir/www/
-   bower install
-   npm install
-   cd $master_dir
-   gradle clean
-   gradle build
-elif [ "$branch_name" != "$release_name" ]
-then
-  cd $os_core
-  git checkout master
-  git pull
-  exists=`git ls-remote --heads origin $branch_Name`
-  if [ -n "$exists" ]; then
-   echo "Branch Name Exists"
-   git checkout $branch_Name
-   git pull
-   git tag -a $release_name -m "OpenSpecimen release $release_name"
-   git checkout $release_name
-   cd $os_core/www/
-   bower install
-   npm install
-   cd $os_core
-   gradle clean
-   gradle build
-   git push git@github.com:krishagni/openspecimen.git $release_name
-   cd
-   else
-      echo "Branch Name Does Not Exist"
-      git checkout master
-      git pull
-      git tag -a $release_name -m "OpenSpecimen release $release_name"
-      git checkout $release_name
-      cd $os_core/www/
-      bower install
-      npm install
-      cd $os_core
-      gradle clean
-      gradle build
-      git push git@github.com:krishagni/openspecimen.git $release_name
-    fi
+   echo "branch doesn't exist"
+   exit 0;
 fi
+
+git checkout $branch_name
+git pull
+
+if [ ! -z $release_name ]
+then 
+    git tag -a $release_name -m "OpenSpecimen release $release_name"
+    git checkout $release_name
+fi
+
+cd $master_dir/www/
+bower install
+npm install
+cd $master_dir
+gradle clean
+gradle build
+
+if [ ! -z $release_name ]
+then       	
+  git push git@github.com:krishagni/openspecimen.git $release_name
+fi
+
