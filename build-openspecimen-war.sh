101fa7f833d798c6849ced4647fcbb9e6647f024
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2022-03-10 15:18:34 +0530

    updated script for plugins as well

--- build-openspecimen-war.sh
+++ build-openspecimen-war.sh
@@ -3,16 +3,17 @@
 repo_dir=$1
 branch_name=$2
 release_name=$3
+core_app="/mnt/volume_nyc1_01/new_workspace/os_source/openspecimen"
+component="./src/main/"
 
 if [ -z $branch_name ] || [ ! -d $repo_dir ]
 then
-   echo "1. If you releasing only master build the please provide only master branch name with command line"
-   echo "2. If you releasing the version the please provide a. branch name b. release name with command line"
+   echo "1. If you releasing only master build the please provide repo_dir path and branch name. "
+   echo "2. If you releasing the version the please provide a. repo_dir b. branch name and c. release name with command line"
    exit 0;
 fi
 
 npm cache clean --force
-/home/jenkins/jenkins_home/workspace/os_source/jenkins_script/remove-links.sh
 
 cd $repo_dir
 
@@ -32,10 +33,23 @@ then
     git checkout $release_name
 fi
 
-cd $repo_dir/www/
-bower install
-npm install
-cd $repo_dir
+cd "$repo_dir"
+
+if [ -d "$repo_dir/WEB-INF" ]
+then	
+   cd "$repo_dir/www/"
+   bower install
+   npm install
+else 
+   ln -sf "$core_app/www/bower_components" "$component/webapp/bower_components"
+   ln -sf "$core_app/www/node_modules" "$component/webapp/node_modules"
+   ln -sf "$core_app/www/external_components" "$component/webapp/external_components"
+   if [ -d "./src/main/ui" ]
+   then
+     ln -sf "$core_app/ui/node_modules" "$componentui/ui/node_modules"
+   fi
+fi
+
 gradle clean
 gradle build
 
