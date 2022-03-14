9c1faeea90a56b39f960c9dc07eaa70fc5b4c85b
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2022-03-14 14:56:43 +0530

    Added the jar validation code

--- master-zip-creation.sh
+++ master-zip-creation.sh
@@ -2,7 +2,7 @@
 
 os_source=$1
 base_folder=$2
-config_file=$3
+config_file=$2
 datestamp=$(date +%d-%m-%Y)
 
 if [ -z $os_source ] || [ -z $base_folder ] || [ -z $config_file ]
@@ -16,12 +16,17 @@ mkdir $base_folder-$datestamp $base_folder-$datestamp/plugin_build/
 cp -r $os_source/build-tools/* $base_folder-$datestamp
 
 cp $os_source/openspecimen/build/libs/openspecimen.war $base_folder-$datestamp
-rm $workspace/os-extras/build/libs/os-extras.jar
 
-while read plugin; do
-   cp $os_source/$plugin/build/libs/*.jar $base_folder-$datestamp/plugin_build
+while IFS=, read -r plugin plugin_file_name; do
+  if [ -f $os_source/$os_source/$plugin/build/libs/$plugin_file_name ]
+  then
+    cp $os_source/$plugin/build/libs/$plugin_file_name $base_folder-$datestamp/plugin_build
+  else
+    echo "Plugin not found"
+    exit 0;
+  fi  
 done <$config_file
 
-#Creating master build zip containing all required files.
+Creating master build zip containing all required files.
 zip -r $base_folder-$datestamp.zip $base_folder-$datestamp
 rm -r $base_folder-$datestamp/
