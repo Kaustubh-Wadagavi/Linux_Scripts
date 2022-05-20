1e7dea92cda507c625c99ce9e4d10c79541a8eb5
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2022-05-20 15:04:09 +0530

    Updated script with checking the PID, and added if PID is not exist the exit

--- startOpenSpecimen.sh
+++ startOpenSpecimen.sh
@@ -1,40 +1,72 @@
 #!/bin/bash
 
-URL=http://localhost:8080/openspecimen
-COUNT=1
-RETRY_COUNT=5
+URL=https://build.openspecimen.org/openspecimen
+TOMCAT_HOME=/usr/local/openspecimen/tomcat-as
 SERVICE_NAME=openspecimen
 PASSWORD='Krish!@3agni'
 
+restartServer() {
+  echo $PASSWORD | sudo -S systemctl start $SERVICE_NAME
+
+}
+
+checkProcess() {
+  if (( $(ps -ef | grep -v grep | grep $SERVICE_NAME | wc -l) > 0 ))
+  then
+      kill -9 $(ps -ef | grep $SERVICE_NAME | grep -v grep | awk '{print $2}')
+      return 0;
+  else
+      return 1;
+  fi
+
+}
+
+checkPid() {
+  if [ -f "$TOMCAT_HOME/bin/pid.txt" ]
+  then 
+      return 0;
+  else 
+      return 1;
+  fi 
+
+}
+
 invokeApi() {
-  wget -T 60 --no-check-certificate $URL/rest/ng/config-settings/app-props
-  STATUS=$?
-  if [ $STATUS -eq 0 ]
-  then
-    echo "App is running.."
-    exit 0
-  fi
+  wget --no-check-certificate -o applog  $URL/rest/ng/config-settings/app-props
+  return $?;
+
 }
 
-loadConfigProperties() {
-  if [ $COUNT -gt $RETRY_COUNT ]
-  then
-    CHECK_SERVICE_RUNNING_STATUS=$(ps -ef | grep -v grep | grep $SERVICE_NAME | wc -l)
-    if [ $CHECK_SERVICE_RUNNING_STATUS -gt 0 ]
-    then 
-	echo $PASSWORD | sudo -S systemctl restart $SERVICE_NAME
-	sleep 120
-    elif [ $CHECK_SERVICE_RUNNING_STATUS -eq 0 ]
+main() {
+  for ((COUNT=0; $COUNT <= 5; COUNT++))
+  do
+   invokeApi
+    STATUS=$?
+    if [ $STATUS -eq 0 ]
     then
-	echo "Someone stopped the OpenSpecimen for maintainance...."
-	exit 0
+      echo "App is running..."
+      exit 0;
     fi
+    sleep 10;
+  done
+
+  checkPid
+  PID_EXISTS=$?
+  if [ $PID_EXISTS -eq 0 ]
+  then
+    checkProcess
+  else
+    echo "Someone Stopped OpenSpecimen...!"
+    exit 0;
   fi
-  invokeApi
-  sleep 10
-  ((COUNT=COUNT+1))
-  echo $COUNT
-  loadConfigProperties
+
+  PROCESS_STATUS=$?
+  if [ $processStatus -eq 0 ]
+  then
+    restartServer
+    exit 0;
+  fi
+
 }
 
-loadConfigProperties
+main;
