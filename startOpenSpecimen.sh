6dc37ee59483ad0540a93ecda592e3b6e9ea3cd8
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2022-05-26 11:38:20 +0530

    Enhancements: 1. Added email functionality. 2. Added Heap Dump

--- startOpenSpecimen.sh
+++ startOpenSpecimen.sh
@@ -4,16 +4,44 @@ URL=https://build.openspecimen.org/openspecimen
 TOMCAT_HOME=/usr/local/openspecimen/tomcat-as
 SERVICE_NAME=openspecimen
 PASSWORD='Krish!@3agni'
+EMAIL_ID='do-not-reply@krishagni.com'
+EMAIL_PASS='fmhrtiydtzqnfyke'
+CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
+EMAIL_FILE=$CURRENT_TIME.txt
+RCPT_EMAIL_ID='build@krishagni.com'
+CLIENT_NAME_AND_ENVIRONMENT="Kaustubh Local Instance"
+HEAP_DUMP_FILE=$CURRENT_TIME.hprof
+
+sendMail() {
+  touch $EMAIL_FILE
+  cat > $EMAIL_FILE << EOF
+Subject: [ IMPORTANT ALERT ] : $CLIENT_NAME_AND_ENVIRONMENT
+        
+Hello Build Team,
+ 
+   The server is restarted.
+
+   Please check the server performance.
+
+Regards,
+Auto-Restart-Bot
+EOF
+
+  curl --ssl-reqd --url 'smtps://smtp.gmail.com:465' -u $EMAIL_ID:$EMAIL_PASS --mail-from $EMAIL_ID --mail-rcpt $RCPT_EMAIL_ID --upload-file $EMAIL_FILE
+
+}
 
 restartServer() {
   echo $PASSWORD | sudo -S systemctl start $SERVICE_NAME
 
 }
 
-checkProcess() {
+takeHeapDumpAndKill() {
+  PROCESS_ID=$(ps -ef | grep $SERVICE_NAME | grep -v grep | awk '{print $2}')
   if (( $(ps -ef | grep -v grep | grep $SERVICE_NAME | wc -l) > 0 ))
   then
-      kill -9 $(ps -ef | grep $SERVICE_NAME | grep -v grep | awk '{print $2}')
+      jmap -dump:live,format=b,file=$HEAP_DUMP_FILE $PROCESS_ID
+      kill -9 $PROCESS_ID
       return 0;
   else
       return 1;
@@ -32,14 +60,16 @@ checkPid() {
 }
 
 invokeApi() {
+  echo "Inside Invoke"
   wget --no-check-certificate -o applog  $URL/rest/ng/config-settings/app-props
   return $?;
 
 }
 
 main() {
-  for ((COUNT=0; $COUNT <= 5; COUNT++))
+  for ((COUNT=0; $COUNT <= 1; COUNT++))
   do
+   echo "Inside For"
    invokeApi
     STATUS=$?
     if [ $STATUS -eq 0 ]
@@ -54,18 +84,20 @@ main() {
   PID_EXISTS=$?
   if [ $PID_EXISTS -eq 0 ]
   then
-    checkProcess
+    takeHeapDumpAndKill
   else
     echo "Someone Stopped OpenSpecimen...!"
     exit 0;
   fi
 
   PROCESS_STATUS=$?
-  if [ $processStatus -eq 0 ]
+  if [ $PROCESS_STATUS -eq 0 ]
   then
     restartServer
-    exit 0;
   fi
+  
+  sendMail
+  exit 0;
 
 }
 
