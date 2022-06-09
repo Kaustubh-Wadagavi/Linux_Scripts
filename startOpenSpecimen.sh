b184ab9f775610c443eab851d68825dede9606ce
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2022-06-09 10:01:51 +0530

    Enhancements: 1. Script made to run as service user 2. Added log files attachments to the mail

--- startOpenSpecimen.sh
+++ startOpenSpecimen.sh
@@ -1,46 +1,39 @@
 #!/bin/bash
 
 URL=
-TOMCAT_HOME=/usr/local/openspecimen/tomcat-as
-SERVICE_NAME=openspecimen
-PASSWORD='Krish!@3agni'
+TOMCAT_HOME=
+OS_DATA=
+SERVICE_USER=
+SERVICE_NAME=
 EMAIL_ID=
 EMAIL_PASS=
 CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
 EMAIL_FILE=$CURRENT_TIME.txt
 RCPT_EMAIL_ID=
 CLIENT_NAME_AND_ENVIRONMENT="Kaustubh Local Instance"
-HEAP_DUMP_FILE=$CURRENT_TIME.hprof
+HEAP_DUMP_FILE=$TOMCAT_HOME/bin/$CURRENT_TIME.hprof
+
 
 sendMail() {
-  touch $EMAIL_FILE
-  cat > $EMAIL_FILE << EOF
-Subject: [ IMPORTANT ALERT ] : $CLIENT_NAME_AND_ENVIRONMENT
-        
-Hello Build Team,
- 
-   The server is restarted.
-
-   Please check the server performance.
-
-Regards,
-Auto-Restart-Bot
-EOF
-  curl --ssl-reqd --url 'smtps://smtp.gmail.com:465' -u $EMAIL_ID:$EMAIL_PASS --mail-from $EMAIL_ID --mail-rcpt $RCPT_EMAIL_ID --upload-file $EMAIL_FILE
+curl --ssl-reqd --url 'smtps://smtp.gmail.com:465' -u $EMAIL_ID:$EMAIL_PASS --mail-from $EMAIL_ID --mail-rcpt $RCPT_EMAIL_ID -H "Subject: [ IMPORTANT ALERT ] : $CLIENT_NAME_AND_ENVIRONMENT" -F text="Hello Build Team, "$'\n'""$'\n'"The $CLIENT_NAME_AND_ENVIRONMENT restared. "$'\n'""$'\n'"The heap dump file location is: $HEAP_DUMP_FILE  "$'\n'""$'\n'"Regards, "$'\n'"auto-restart" -F attachment='@logs.zip'
 
 }
 
 restartServer() {
-  echo $PASSWORD | sudo -S systemctl start $SERVICE_NAME
+  $TOMCAT_HOME/bin/shutdown.sh -force
+  $TOMCAT_HOME/bin/startup.sh
 
 }
 
-takeHeapDumpAndKill() {
+takeHeapDumpAndLogs() {
   PROCESS_ID=$(cat $TOMCAT_HOME/bin/pid.txt)
   if (( $(ps -ef | grep -v grep | grep $SERVICE_NAME | wc -l) > 0 ))
   then
       jmap -dump:live,format=b,file=$HEAP_DUMP_FILE $PROCESS_ID
-      kill -9 $PROCESS_ID
+      cp $TOMCAT_HOME/logs/catalina.out .
+      cp $OS_DATA/logs/os.log .
+      zip -r logs.zip catalina.out os.log
+      rm -r catalina.out os.log
       return 0;
   else
       return 1;
@@ -59,7 +52,6 @@ checkPid() {
 }
 
 invokeApi() {
-  echo "Inside Invoke"
   wget --no-check-certificate -o applog  $URL/rest/ng/config-settings/app-props
   return $?;
 
@@ -68,7 +60,6 @@ invokeApi() {
 main() {
   for ((COUNT=0; $COUNT <= 1; COUNT++))
   do
-   echo "Inside For"
    invokeApi
     STATUS=$?
     if [ $STATUS -eq 0 ]
@@ -83,7 +74,7 @@ main() {
   PID_EXISTS=$?
   if [ $PID_EXISTS -eq 0 ]
   then
-    takeHeapDumpAndKill
+    takeHeapDumpAndLogs
   else
     echo "Someone Stopped OpenSpecimen...!"
     exit 0;
