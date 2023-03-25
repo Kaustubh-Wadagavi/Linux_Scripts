cc4e6b8442598834f4b84420a77c88512ea65ba2
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2023-03-25 12:17:39 +0530

    Ccing multiple peoples in mail code added

--- sendSupportReport.sh
+++ sendSupportReport.sh
@@ -5,18 +5,18 @@ configFile=$1
 sendEmail() {
   currentMonth=$(date '+%B_%Y')
   tempFile=tempfile.txt
-  echo -e "To: $emailId\nSubject: OpenSpecimen/$shortName: Monthly support log for $currentMonth\n\n \
+  echo -e "To: $emailId\ncc:$ccMail1,$ccMail2\nSubject: OpenSpecimen/$shortName: Monthly support log for $currentMonth\n\n \
   Hello $projectManager,\n\n \
   Below is the summary of the OpenSpecimen support activities.\n\n \
-  Type of Support Package: $supportPackage\n \
+  Type of Support Package: $contractType\n \
   Start date of contract: $startDateOfContract\n \
   End date of contract: $endDateOfContract\n \
   Total credits: $totalCredits\n \
   Used credits: $usedCredits\n\n \
   Please find below details of support tickets:\n\n$(cat $reportFile)" > $tempFile
 
-  curl --ssl-reqd --url 'smtps://smtp.gmail.com:465' -u $fromEmail:$emailPassword --mail-from $fromEmail -v --mail-rcpt $emailId --mail-rcpt $fromEmail --upload-file $tempFile
-  rm $tempFile
+  curl --ssl-reqd --url 'smtps://smtp.gmail.com:465' -u $fromEmail:$emailPassword --mail-from $fromEmail -v --mail-rcpt $emailId --upload-file $tempFile
+  rm -f $tempFile
 
 }
 
@@ -49,19 +49,21 @@ getToken() {
 }
 
 copySendingFiles() {
-   cp $sourceDir/*.csv $destDir
+   if [ -d $destDir ]; then rm -r $destDir; fi
+   mkdir -p $destDir
+   cp $sourceDir/*.csv $destDir/
+
 }
 
 main() {
   if [ ! -f "$configFile" ]
   then
-    echo insideif
     echo "Please input the config file"
     exit 0;
   fi
   
   source $configFile
- # copySendingFiles
+#  copySendingFiles
   getToken
   getClientsEmailId
 
