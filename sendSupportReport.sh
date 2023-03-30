49b4dbc69663ff93b89f6ce0535fea76b94a97f0
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2023-03-30 18:25:34 +0530

    Added the annoncement variable

--- sendSupportReport.sh
+++ sendSupportReport.sh
@@ -4,27 +4,37 @@ configFile=$1
 
 sendEmail() {
   currentMonth=$(date '+%B_%Y')
-  tempFile=tempfile.txt
-  toEmail="kaustubh@krishagni.com, kaustubhwadagavi@gmail.com" # Add multiple email addresses separated by comma
-  if [[ $toEmail == *,* ]]; then
-   mail1=$(echo $toEmail | cut -d ',' -f1)
-   mail2=$(echo $toEmail | cut -d ',' -f2)
- else
-   var1=$toEmail
- fi
-  echo -e "To: $mail1, $mail2\ncc: $ccEmail1, $ccEmail2\nSubject: OpenSpecimen/$shortName: Monthly support log for $currentMonth\n\n \
-  Hello $projectManager,\n\n \
-  Below is the summary of the OpenSpecimen support activities.\n\n \
-  Type of Support Package: $contractType\n \
-  Start date of contract: $startDateOfContract\n \
-  End date of contract: $endDateOfContract\n \
-  Total credits: $totalCredits\n \
-  Used credits: $usedCredits\n\n \
-  Please find below details of support tickets: \
-  $(echo "$announcement" | sed -e 's/<[^>]*>//g' -e 's/^\s*//' -e 's/<br\s*\/>/\n/g')\n\n$(cat $reportFile)" > $tempFile
+tempFile=tempfile.txt
+toEmail="kaustubh@krishagni.com, kaustubhwadagavi@gmail.com" # Add multiple email addresses separated by comma
+if [[ $toEmail == *,* ]]; then
+  mail1=$(echo $toEmail | cut -d ',' -f1)
+  mail2=$(echo $toEmail | cut -d ',' -f2)
+else
+  mail1=$toEmail
+fi
+announcement="<ol>
+    <li><a href='https://openspecimen.atlassian.net/wiki/x/BYDWmg'><span style='background-color:hsl(0, 0%, 100%);color:hsl(0, 75%, 60%);'><strong>v10.1 released</strong></span></a></li>
+    <li><a href='https://openspecimen.org/oscon23'><span style='background-color:hsl(0, 0%, 100%);color:hsl(210, 75%, 60%);'><strong>Register for OSCON23&nbsp;(23-26 July, San Diego)</strong></span></a></li>
+    <li><span style='background-color:hsl(0, 0%, 100%);color:hsl(30, 75%, 60%);font-family:Arial, Helvetica, sans-serif;'><strong>Kaustubh Wadagavi is getting married</strong></span></li>
+</ol>"
 
-  curl --ssl-reqd --url 'smtps://smtp.gmail.com:465' -u $fromEmail:$emailPassword --mail-from $fromEmail -v --mail-rcpt $mail1 --mail-rcpt $mail2 --upload-file $tempFile
-  rm -f $tempFile
+reportFileHtml=$(awk -F',' 'BEGIN { print "<table border=1 cellpadding=5 cellspacing=0>"} {print "<tr>"; for(i=1;i<=NF;i++) {if (NR==1) printf "<th><b><font color=\"blue\">%s</font></b></th>", $i; else printf "<td>%s</td>", $i} print "</tr>"} END{print "</table>"}' $reportFile)
+
+echo -e "To: $mail1, $mail2\ncc: $ccEmail1, $ccEmail2\nSubject: OpenSpecimen/$shortName: Monthly support log for $currentMonth\nContent-Type: text/html\n\n \
+Hello $projectManager,<br><br> \
+Below is the summary of the OpenSpecimen support activities.<br><br> \
+<b>Type of Support Package:</b> $contractType<br> \
+<b>Start date of contract:</b> $startDateOfContract<br> \
+<b>End date of contract:</b> $endDateOfContract<br> \
+<b>Total credits:</b> $totalCredits<br> \
+<b>Used credits:</b> $usedCredits<br><br> \
+<b>Please find below details of support tickets:</b><br><br> \
+<font color='red'><b>Here are some interesting OpenSpecimen news for this month: </b></font><br> \
+$(echo "$announcement")<br><br> \
+$reportFileHtml" > $tempFile
+
+curl --ssl-reqd --url 'smtps://smtp.gmail.com:465' -u $fromEmail:$emailPassword --mail-from $fromEmail -v --mail-rcpt $mail1 --mail-rcpt $mail2 --upload-file $tempFile
+rm -f $tempFile
 
 }
 
@@ -46,7 +56,7 @@ getClientsEmailId() {
      totalCredits=`echo ${getSupportDetails} | jq -r '.participant.extensionDetail.attrs[14].displayValue'`
      usedCredits=`echo ${getSupportDetails} | jq -r '.participant.extensionDetail.attrs[15].displayValue'`
      ignore=`echo ${getSupportDetails} | jq -r '.participant.extensionDetail.attrs[16].displayValue'`
-     announcement=`echo ${getSupportDetails} | jq -r '.participant.extensionDetail.attrs[17].displayValue'`
+#     announcement=`echo ${getSupportDetails} | jq -r '.participant.extensionDetail.attrs[17].displayValue'`
      echo $usedCredits
      echo $ignore
      echo $client
