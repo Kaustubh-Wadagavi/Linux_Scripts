ee2e796a9319898d119ad28e7202236ab4a61071
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2023-04-12 18:19:47 +0530

    Fixed bug related to export job

--- sendSupportReport.sh
+++ sendSupportReport.sh
@@ -5,7 +5,9 @@ configFile=$1
 sendEmail() {
   currentMonth=$(date '+%B_%Y')
   tempFile="$inputDir/files/tempfile.txt"
-  toEmail="kaustubh@krishagni.com,kaustubhwadagavi@gmail.com" # Add multiple email addresses separated by comma
+  toEmail="bhimsen@krishagni.com, nilesh@krishagni.com" #$emailId
+  toEmail=$(echo "$toEmail" | tr -d ' ')
+
   if [[ $toEmail == *,* ]]; then
    mail1=$(echo $toEmail | cut -d ',' -f1)
    mail2=$(echo $toEmail | cut -d ',' -f2)
@@ -13,9 +15,9 @@ sendEmail() {
    mail1=$toEmail
   fi
 
-reportFileHtml=$(awk -F',' 'BEGIN { print "<table border=1 cellpadding=5 cellspacing=0>"} {print "<tr>"; for(i=1;i<=NF;i++) { if (NR==1) printf "<th><b><font color=\"blue\">%s</font></b></th>", $i; else printf "<td>%s</td>", $i } print "</tr>"} END{print "</table>"}' <(sed -E 's/^([^,]*,[^,]*,[^,]*,[^,]*,)("[^"]+",)(.*)/\1\2\3/; :a; s/("[^"]*),([^"]*")/\1\2/g; ta' "$inputDir/files/$fileName"))
+reportFileHtml=$(awk -F',' 'BEGIN { print "<table border=1 cellpadding=5 cellspacing=0>"} {print "<tr>"; for(i=1;i<=NF;i++) { if (NR==1 && i!=3) printf "<th><b><font color=\"blue\">%s</font></b></th>", $i; else if (i!=3) printf "<td>%s</td>", $i } print "</tr>"} END{print "</table>"}' <(sed -E 's/^([^,]*,[^,]*,[^,]*,[^,]*,)("[^"]+",)(.*)/\1\2\3/; :a; s/("[^"]*),([^"]*")/\1\2/g; ta' "$inputDir/files/$fileName"))
 
-echo -e "To: $mail1, $mail2\ncc: $ccEmail1, $ccEmail2\nSubject: OpenSpecimen/$shortName: Monthly support log for $currentMonth\nContent-Type: text/html\n\n \
+echo -e "To: $mail1, $mail2\ncc: $ccMail1, $ccMail2\nSubject: OpenSpecimen/$shortName: Monthly support log for $currentMonth\nContent-Type: text/html\n\n \
 Hello $projectManager,<br><br> \
 Below is the summary of the OpenSpecimen support activities.<br><br> \
 <b>Type of Support Package:</b> $contractType<br> \
@@ -30,7 +32,11 @@ Below is the summary of the OpenSpecimen support activities.<br><br> \
 $(echo "$announcement")<br><br> \
 $reportFileHtml" > "$tempFile"
 
-curl --ssl-reqd --url 'smtps://smtp.gmail.com:465' -u "$fromEmail:$emailPassword" --mail-from "$fromEmail" -v --mail-rcpt "$mail1" --mail-rcpt "$mail2" --upload-file "$tempFile"
+if [[ $toEmail == *,* ]]; then
+    curl --ssl-reqd --url 'smtps://smtp.gmail.com:465' -u "$fromEmail:$emailPassword" --mail-from "$fromEmail" -v --mail-rcpt "$mail1" --mail-rcpt "$mail2" --mail-rcpt "$ccMail1" --mail-rcpt "$ccMail2" --upload-file "$tempFile"
+else
+    curl --ssl-reqd --url 'smtps://smtp.gmail.com:465' -u "$fromEmail:$emailPassword" --mail-from "$fromEmail" -v --mail-rcpt "$mail1" --mail-rcpt "$ccMail1" --mail-rcpt "$ccMail2" --upload-file "$tempFile"
+fi
 
 }
 
@@ -55,13 +61,35 @@ getClientDetails() {
      ignore=`echo ${getSupportDetails} | jq -r '.participant.extensionDetail.attrs[16].displayValue'`
 
      getAnnouncement=$(curl -X GET -H "X-OS-API-TOKEN: $os_token" -H "Content-Type: application/json" "$url/rest/ng/collection-protocols/1")
-     announcement=`echo ${getAnnouncement} | jq -r '.extensionDetail.attrs[].value'`
-     currentVersion="v10.1.RC1"   #`echo ${getAnnouncement} | jq -r '.extensionDetail.attrs[].value'`
+     announcement=`echo ${getAnnouncement} | jq -r '.extensionDetail.attrs[0].value'`
+     currentVersion=`echo ${getAnnouncement} | jq -r '.extensionDetail.attrs[1].value'`
      yourCurrentVersion="v9.1.RC1" # `echo ${getAnnouncement} | jq -r '.extensionDetail.attrs[].value'`
      if [[ $ignore != "Yes" ]] && [[ $contractType != "Yet To Go-Live" ]] && [[ $contractType != "Closed" ]] ; then
         sendEmail
      fi
    done
+
+}
+
+downloadFile() {
+  curl -X GET -H "X-OS-API-TOKEN: $os_token" -H "Content-Type: application/json" "$url/rest/ng/export-jobs/$ID/output" > ExportJob_$ID.zip
+  if [ -d "$inputDir" ]; then
+     rm -rf "$inputDir"
+  fi
+  mkdir -p "$inputDir"
+  unzip ExportJob_$ID.zip -d $inputDir
+  rm ExportJob_$ID.zip
+  echo '"id","ppid","fileName"' > "$inputDir/output_file.csv"
+  while IFS="," read line
+  do
+   if [[ "$line" == *".csv"* ]]; then
+     id=$(echo "$line" | cut -d ',' -f 1 | grep -oE '[0-9]+')
+     ppid=$(echo "$line" | cut -d ',' -f 3)
+     fileName=$(echo "$line" | grep -oE '[^",]+\.csv')
+     echo $id,$ppid,$fileName >> "$inputDir/output_file.csv"
+   fi
+  done < "$inputDir/output.csv"
+
 
 }
 
@@ -71,23 +99,9 @@ exportParticipants() {
    ID=`echo ${JOB_DETAILS} | jq -r '.id'`
    STATUS=`echo ${JOB_DETAILS} | jq -r '.status'`
 
-   curl -X GET -H "X-OS-API-TOKEN: $os_token" -H "Content-Type: application/json" "$url/rest/ng/export-jobs/$ID/output" > ExportJob_$ID.zip
-   if [ -d "$inputDir" ]; then
-      rm -rf "$inputDir"
+   if [[ "$STATUS" == "IN_PROGRESS" ]]; then
+       sleep 300
    fi
-   mkdir -p "$inputDir"
-   unzip ExportJob_$ID.zip -d $inputDir
-   rm ExportJob_$ID.zip
-   echo '"id","ppid","fileName"' > "$inputDir/output_file.csv"
-   while IFS="," read line
-   do
-    if [[ "$line" == *".csv"* ]]; then
-        id=$(echo "$line" | cut -d ',' -f 1 | grep -oE '[0-9]+')
-        ppid=$(echo "$line" | cut -d ',' -f 3)
-        fileName=$(echo "$line" | grep -oE '[^",]+\.csv')
-        echo $id,$ppid,$fileName >> "$inputDir/output_file.csv"
-    fi
-  done < "$inputDir/output.csv"
 
 }
 
@@ -105,11 +119,11 @@ main() {
     exit 0;
   fi
   
-  source $configFile
-  getToken
-  exportParticipants
-  getClientDetails
-  sendEmail
+ source $configFile
+ getToken
+ exportParticipants
+ downloadFile
+ getClientDetails
 
 }
 
--- updateCredits.sh
+++ updateCredits.sh
@@ -10,7 +10,28 @@ updateCredits() {
    echo $ppid: $totalCredits
    echo ===============================================================
    curl -X PUT -H "X-OS-API-TOKEN: $os_token" -H "Content-Type: application/json" -X PUT -d '{"participant":{"extensionDetail":{"formCaption":"Krishagni_clients","attrs":[{"name":"used_credits","value":"'"$totalCredits"'"}]}}}' "$url/rest/ng/collection-protocol-registrations/$id"
-done
+ done
+
+}
+
+downloadFile(){
+  curl -X GET -H "X-OS-API-TOKEN: $os_token" -H "Content-Type: application/json" "$url/rest/ng/export-jobs/$ID/output" > ExportJob_$ID.zip
+  if [ -d "$inputDir" ]; then
+     rm -rf "$inputDir"
+  fi
+  mkdir -p "$inputDir"
+  unzip ExportJob_$ID.zip -d $inputDir
+  rm ExportJob_$ID.zip
+  echo '"id","ppid","fileName"' > "$inputDir/output_file.csv"
+  while IFS="," read line
+  do
+   if [[ "$line" == *".csv"* ]]; then
+     id=$(echo "$line" | cut -d ',' -f 1 | grep -oE '[0-9]+')
+     ppid=$(echo "$line" | cut -d ',' -f 3)
+     fileName=$(echo "$line" | grep -oE '[^",]+\.csv')
+     echo $id,$ppid,$fileName >> "$inputDir/output_file.csv"
+   fi
+  done < "$inputDir/output.csv"
 
 }
 
@@ -18,25 +39,11 @@ exportParticipants() {
    JOB_DETAILS=$(curl -X POST -H "X-OS-API-TOKEN: $os_token" -H "Content-Type: application/json" -d '{ "objectType":"'"cpr"'", "params": { "cpId": "'"1"'" } }' "$url/rest/ng/export-jobs")
    
    ID=`echo ${JOB_DETAILS} | jq -r '.id'`
-   STATUS=`echo ${JOB_DETAILS} | jq -r '.status'`
-    
-   curl -X GET -H "X-OS-API-TOKEN: $os_token" -H "Content-Type: application/json" "$url/rest/ng/export-jobs/$ID/output" > ExportJob_$ID.zip
-   if [ -d "$inputDir" ]; then
-      rm -rf "$inputDir"
+   STATUS=`echo ${JOB_DETAILS} | jq -r '.status'` 
+   
+   if [[ "$STATUS" == "IN_PROGRESS" ]]; then
+       sleep 300
    fi
-   mkdir -p "$inputDir"
-   unzip ExportJob_$ID.zip -d $inputDir
-   rm ExportJob_$ID.zip
-   echo '"id","ppid","fileName"' > "$inputDir/output_file.csv"
-   while IFS="," read line
-   do
-    if [[ "$line" == *".csv"* ]]; then
-        id=$(echo "$line" | cut -d ',' -f 1 | grep -oE '[0-9]+')
-        ppid=$(echo "$line" | cut -d ',' -f 3)
-        fileName=$(echo "$line" | grep -oE '[^",]+\.csv')
-        echo $id,$ppid,$fileName >> "$inputDir/output_file.csv"
-    fi
-  done < "$inputDir/output.csv"
 
 }
 
@@ -56,6 +63,7 @@ main() {
   source $configFile
   getToken
   exportParticipants
+  downloadFile
   updateCredits
 
 }
