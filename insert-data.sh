4be9a198a6e49bba0e1d0250a13b2d4b21880260
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2023-06-12 07:35:06 -0500

    Finxed null insertion bug

--- insert-data.sh
+++ insert-data.sh
@@ -8,36 +8,54 @@ DB_CONNECTION=""
 COMMIT_FREQUENCY=100
 counter=0
 
-# Connect to Oracle and set autocommit off
+# Set autocommit off
 sqlplus -S "${DB_USERNAME}/${DB_PASSWORD}@${DB_CONNECTION}" <<EOF
     SET AUTOCOMMIT OFF;
 EOF
 
-# Read CSV file and update records
+# Read CSV file and insert records
+counter=0
 while IFS=',' read -r identifier label created_on created_by last_modified last_modified_by
 do
-    #Need to fix the command 
-    # Connect to Oracle and update the record
-    #sqlplus -S "${DB_USERNAME}/${DB_PASSWORD}@${DB_CONNECTION}" <<EOF
-       # INSERT INTO catissue_abstract_specimen (identifier, label, created_on, created_by, last_modified, last_modified_by)
-     #   VALUES ($identifier, '$label', '$created_on', $created_by, '$last_modified', $last_modified_by);
+    # Handle empty values
+    if [[ -z "$created_on" ]]; then
+        created_on="NULL"
+    else
+        created_on="'$created_on'"
+    fi
+
+    if [[ -z "$created_by" ]]; then
+        created_by="NULL"
+    fi
+
+    if [[ -z "$last_modified" ]]; then
+        last_modified="NULL"
+    else
+        last_modified="'$last_modified'"
+    fi
+
+    if [[ -z "$last_modified_by" ]]; then
+        last_modified_by="NULL"
+    fi
+
+    # Connect to Oracle and insert the record
+    sqlplus -S "${DB_USERNAME}/${DB_PASSWORD}@${DB_CONNECTION}" <<EOF
+        INSERT INTO catissue_abstract_specimen (identifier, label, created_on, created_by, last_modified, last_modified_by)
+        VALUES ($identifier, '$label', $created_on, $created_by, $last_modified, $last_modified_by);
 EOF
 
-    counter=$((counter+1))
+    echo "Inserted record: identifier=$identifier, label=\"$label\", created_on=$created_on, created_by=$created_by, last_modified=$last_modified, last_modified_by=$last_modified_by"
 
-    if ((counter % COMMIT_FREQUENCY == 0)); then
+    ((counter++))
+    if (( counter % 100 == 0 )); then
+        # Commit after every 100 records
         sqlplus -S "${DB_USERNAME}/${DB_PASSWORD}@${DB_CONNECTION}" <<EOF
             COMMIT;
 EOF
-        echo "Committed after $counter records"
     fi
-
-    echo "Inserted record: identifier=$identifier, label=$label, created_on=$created_on, created_by=$created_by, last_modified=$last_modified, last_modified_by=$last_modified_by"
 done < "$CSV_FILE"
 
-# Commit remaining records
+# Commit any remaining records
 sqlplus -S "${DB_USERNAME}/${DB_PASSWORD}@${DB_CONNECTION}" <<EOF
     COMMIT;
 EOF
-
-echo "Committed all records"
