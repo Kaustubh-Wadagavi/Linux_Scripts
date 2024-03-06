e819454a6479aba6cfc10afc031553e0d3113cc6
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2024-03-06 15:58:12 +0530

    1. Removed recursion, 2. Added more validations 3. To download file calling API once.

--- export-query-data/export-query-data.sh
+++ export-query-data/export-query-data.sh
@@ -1,41 +1,52 @@
 #!/bin/bash
 
-configFile=$1
-
 getFile() {
-  getStatusCode=$(curl -s -o /dev/null -w "%{http_code}" -H "X-OS-API-TOKEN: $token" -H "Content-Type: application/json" --request GET "$url/rest/ng/query/export?fileId=$dataFileId")
-  if [[ "$getStatusCode" -eq "400" ]]
-  then
-      echo "Please wait the export of the file is still running..."
+  while true; do
+    curl -s -i -H "X-OS-API-TOKEN: $token" -H "Content-Type: application/json" -X GET "$url/rest/ng/query/export?fileId=$dataFileId" -o "output_file"
+ 
+    statusCode=$(awk 'NR==1{print $2}' output_file)
+    
+    if [[ "$statusCode" == "400" ]]
+    then
+       jsonObject=$(perl -0777 -ne 'print $1 if /\[(.*?)\]/s' output_file)
+       code=$(echo "$jsonObject" | jq -r '.code')
+       message=$(echo "$jsonObject" | jq -r '.message')
+    fi 
+
+    if [[ "$code" == "QUERY_EXPORT_DATA_IN_PROGRESS" && "$statusCode" == "400" ]]; then
+      echo "Message: $message"
       sleep 10;
-      getFile
-  elif [[ "$getStatusCode" -eq "200" ]]
-  then
-      curl -H "X-OS-API-TOKEN: $token" -H "Content-Type: application/json" -X GET "$url/rest/ng/query/export?fileId=$dataFileId" >> "$outputFile"
+    elif [[ "$statusCode" == "200" ]]; then
+      mv output_file "$outputFile"
       echo "The query output is stored in a file: $outputFile"
-  fi
-  
+      break;
+    elif [[ "$code" != "QUERY_EXPORT_DATA_IN_PROGRESS" && "$statusCode" == "400" ]]; then 
+      echo "Error: $jsonObject"
+      exit 1;
+    fi
+  done
+
 }
 	
 exportQuery() {
-  getDataFile=$(curl -H "X-OS-API-TOKEN: $token" -H "Content-Type: application/json" -X POST --data '{ "savedQueryId": '$queryId',"drivingForm": "'"$drivingForm"'","cpId":-1
-,"runType": "Export","aql": "'"$aql"'","indexOf": "Specimen.label","wideRowMode": "'"$wideRowMode"'","outputColumnExprs": false,"caseSensitive": false
+  getDataFile=$(curl -H "X-OS-API-TOKEN: $token" -H "Content-Type: application/json" -X POST --data '{ "savedQueryId": '$queryId',"drivingForm": "'"$drivingForm"'","cpId":-1,"runType": "Export","aql": "'"$aql"'","indexOf": "Specimen.label","wideRowMode": "'"$wideRowMode"'","outputColumnExprs": false,"caseSensitive": false
 }' "$url/rest/ng/query/export")
+  
   dataFileId=$(echo "$getDataFile" | jq -r '.dataFile')
-  echo $dataFileId
 
   if [[ -z "$dataFileId" ]]
   then
-    echo "Something went wrong, please check the JSON object that you are seding"
+    echo "Error: $getDataFile"
     exit 1;
   fi
 
 }
 
-
 startSession() {
   sessions=$(curl -H "Content-Type: application/json" --request POST --data '{"loginName": "'"$username"'","password":"'"$password"'","domainName":"'"openspecimen"'"}' "$url/rest/ng/sessions")
+  
   token=`echo ${sessions} | jq -r '.token'`
+  
   if [[ -z "$token" ]]
   then
      echo "Please enter valid credentials..."
@@ -45,23 +56,35 @@ startSession() {
 }
 
 checkInputs() {
+  local required=("username" "password" "url" "aql" "queryId" "drivingForm" "wideRowMode")
+  
+  for arg in "${required[@]}"; do
+    local value="${!arg}"
+    if [[ -z "$value" ]]; then
+      echo "Error: $arg is missing."
+      exit 1
+    fi
+  done  
+
+}
+
+main () {
+  configFile=$1
+  
   if [ -z "$configFile" ]
   then
     echo "echo "Error: configFile argument is missing.""
     echo "Usage: ./export-query-data.sh query-details.config"
     exit 1;
-  else
-    source $configFile
   fi
-  
-}
 
-main () {
-  checkInputs
+  source "$configFile"
+  checkInputs "$username" "$password" "$url" "$aql" "$queryId" "$drivingForm" "$wideRowMode"
   startSession
   exportQuery
   getFile
+  exit 0;
 
 }
 
-main;
+main "$@";
