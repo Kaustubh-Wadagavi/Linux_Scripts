fca974e7815788512d63d21933049c4a2907b149
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2024-03-20 12:34:11 +0530

    Added to pick last months start & end date.

--- downloadAuditReport.sh
+++ downloadAuditReport.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-userName=""                                                 # Username to call the REST APIs.
-password=""                                                 # Password of the user.
-url=""                                                      # Host Url.
-startDate="2022-12-13"                                      # Start date 
-endDate="2022-12-20"                                        # End Date
+userName=""                                                                                      # Username to call the REST APIs.
+password=""                                                                                      # Password of the user.
+url="http://localhost:8080/openspecimen"                                                         # Host Url.
+startDate=$(date -d "$(date -d "$(date +%Y-%m-15) -1 month")" "+%Y-%m-01")                       # picks the last months start date
+endDate=$(date -d "$(date +%Y-%m-01) -1 day" "+%Y-%m-%d")                                        # picks the last months end date
 currentTime=$(date "+%Y%m%d_%H%M")
 
 getAuditFile() {
   if [ "$file" = null ]
   then
-    echo "You will recieve and email in a few minutes to download the audit report"
+    echo "You will recieve an email with download link in a few minutes to download the audit report"
   else   
       curl -H "X-OS-API-TOKEN: $apiToken" -H "Content-Type: Content-Type: application/zip" -X GET "$url/rest/ng/audit/revisions-file?fileId=$file" > os_audit_revisions_$currentTime.zip
   fi
@@ -18,7 +18,9 @@ getAuditFile() {
 }
 
 exportAudit() {
-   fileId=$(curl -H "X-OS-API-TOKEN: $apiToken" -H "Content-Type: application/json" -X POST -d '{"includeModifiedProps": "'"true"'","startDate":"'"$startDate"'","endDate":"'"$endDate"'","reportTypes":["'"data"'","'"query_exim"'","'"api_calls"'","'"auth"'"]}' "$url/rest/ng/audit//export-revisions")
+   startDateFormatted=$(date -d "$startDate" "+%Y-%m-%d")
+   endDateFormatted=$(date -d "$endDate" "+%Y-%m-%d")
+   fileId=$(curl -H "X-OS-API-TOKEN: $apiToken" -H "Content-Type: application/json" -X POST -d '{"includeModifiedProps": "'"true"'","startDate":"'"$startDateFormatted"'","endDate":"'"$endDateFormatted"'","reportTypes":["'"data"'","'"query_exim"'","'"api_calls"'","'"auth"'"]}' "$url/rest/ng/audit//export-revisions")
    file=`echo ${fileId} | jq -r '.fileId'`
 
 }
@@ -26,9 +28,19 @@ exportAudit() {
 getToken() {
   sessions=$(curl -H "Content-Type: application/json" -X POST -d '{"loginName": "'"$userName"'","password":"'"$password"'","domainName":"'"openspecimen"'"}' "$url/rest/ng/sessions")
   apiToken=`echo ${sessions} | jq -r '.token'`
+  
+  if [ -z "$apiToken" ]; then
+    echo "Please enter valid credentials.."
+    exit 1
+  fi
 
 }
 
-getToken
-exportAudit
-getAuditFile
+main () {
+ 
+  getToken
+  exportAudit
+  getAuditFile
+}
+
+main;
